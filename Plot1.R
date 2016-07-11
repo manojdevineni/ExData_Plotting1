0225562cbd886de3bd57ffbf6b2fa06b6e15af29
Manoj <devinenimanoj@gmail.com>
2016-07-11 10:51:10 +0530

    Add files via upload

--- Plot1.R
+++ Plot1.R
@@ -1,7 +1,7 @@
 sourcedata <- "D:/DataScience/household_power_consumption.txt"
 data <- read.table(sourcedata, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 reqdata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-global_Active_Power <- as.numeric(subSetData$Global_active_power)
+global_active_power <- as.numeric(reqdata$Global_active_power)
 png("plot1.png", width=480, height=480)
-hist(global_Active_Power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+hist(global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
